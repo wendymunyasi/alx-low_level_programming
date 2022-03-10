d184ffa434a54a5d999c1d445ccaf3a371205fa1
wendy munyasi <wendymunyasi@gmail.com>
2022-03-10 16:46:36 +0300

    Add a script that displays the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ long int b;
 long long int c;
 float d;
 
-printf("Size of char: %d byte(s)\n", sizeof(d));
-printf("Size of int: %d byte(s)\n", sizeof(f));
-printf("Size of long int: %d byte(s)\n", sizeof(b));
-printf("Size of long long int: %d byte(s)\n", sizeof(c));
-printf("Size of float: %d byte(s)\n", sizeof(d));
+printf("Size of a char: %d byte(s)\n", sizeof(e));
+printf("Size of an int: %d byte(s)\n", sizeof(f));
+printf("Size of a long int: %d byte(s)\n", sizeof(b));
+printf("Size of a long long int: %d byte(s)\n", sizeof(c));
+printf("Size of a float: %d byte(s)\n", sizeof(d));
 return (0); }
