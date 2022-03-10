e9b3e0a3876c65b7dafc59a3a37475854c787ef5
wendy munyasi <wendymunyasi@gmail.com>
2022-03-10 23:29:58 +0300

    Last try for C program outputting to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-h
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
+
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(str, sizeof(str), 1, stderr);
+return (1); }
