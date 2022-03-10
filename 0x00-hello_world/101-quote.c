3bb18d6e8498656edcefd3634e716ac2560528fc
wendy munyasi <wendymunyasi@gmail.com>
2022-03-10 23:52:40 +0300

    Last try for C program outputting to std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fwrite(str, sizeof(str), 1, stderr);
 return (1); }
