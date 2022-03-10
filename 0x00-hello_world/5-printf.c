182acf4e8cca72bb7b09d07eeb398f82290ebf3f
wendy munyasi <wendymunyasi@gmail.com>
2022-03-10 15:51:08 +0300

    Follow betty guidelines

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ * Description: prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0
+*/
+
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0); }
