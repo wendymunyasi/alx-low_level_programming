ecbe8894180edb3a09f5c998d455f506df3461c3
wendy munyasi <wendymunyasi@gmail.com>
2022-03-10 15:32:36 +0300

    Edit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+
+/**
  * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Description: prints "Programming is like building a multilingual puzzle"
  * Return: 0
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}
+puts("\"Programming is like building a multilingual puzzle");
+return (0); }
