f638332efef2ce970b32260cab6aa2a7d57f4889
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 14:39:51 +0000

    Add a C program that prints alphabets small and capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: prints all alphabet in lower case
+ * Return: 0
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+    for (ch = 'A'; ch<= 'Z'; ch++)
+        putchar(ch);
+putchar('\n');
+return (0); }
