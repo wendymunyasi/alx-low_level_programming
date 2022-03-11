8b6a64eb8d3fa45b8b6efb62acc4d61f29f05754
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 13:09:12 +0000

    Add C program that prints last digit value

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
+
+int main(void)
+{
+int n;
+int last_digit;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_digit = abs(n % 10);
+if (last_digit > 5)
+{
+printf("Last digit of %i is %d and is greater than 5\n", n, last_digit);
+}
+else if (n == 0)
+{
+printf("Last digit of %i is %d and is 0\n", n, last_digit);
+}
+else if (n < 6 && n != 0)
+{
+printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_digit);
+}
+return (0); }
