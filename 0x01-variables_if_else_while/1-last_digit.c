fad7eed9b24372ef7a6bac18b3e94d72d7fed793
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 13:19:55 +0000

    Fix errors in last_digit code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ if (last_digit > 5)
 {
 printf("Last digit of %i is %d and is greater than 5\n", n, last_digit);
 }
-else if (n == 0)
+else if (last_digit == 0)
 {
 printf("Last digit of %i is %d and is 0\n", n, last_digit);
 }
-else if (n < 6 && n != 0)
+else if (last_digit < 6 && last_digit != 0)
 {
 printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_digit);
 }
