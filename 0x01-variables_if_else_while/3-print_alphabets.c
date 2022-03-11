f00d047a81ddec1aaa694114e46764d3e8822b88
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 14:43:18 +0000

    Fix betty coding style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
-    for (ch = 'A'; ch<= 'Z'; ch++)
-        putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
 putchar('\n');
 return (0); }
