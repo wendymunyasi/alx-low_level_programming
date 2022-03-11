8f8c649c2b5192fab5a5da76582db0906fce3f20
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 14:46:26 +0000

    Write program that prints letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: prints all alphabet in lower case
+ * Return: 0
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0); }
