64248208e9df051f052fbd8288541218fae8c4df
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 13:37:12 +0000

    Add C program that prints all alphabet is lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all alphabet in lower case
+ * Return: 0
+*/
+
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+return (0); }
