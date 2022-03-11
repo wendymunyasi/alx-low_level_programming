491a057154a1efc67b5362ebf55ac7fcc984a2f0
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 20:05:59 +0000

    Add a program that prints nums 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Description: prints all alphabet in lower case
+ * Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+printf("%i", i);
+printf("\n");
+return (0); }
