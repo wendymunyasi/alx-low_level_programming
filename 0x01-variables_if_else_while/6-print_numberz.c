90b433739bd3b6ec3a6ea6b3a4c07a363344ed63
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 20:13:59 +0000

    Fix betty style

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Entry point
- * Description: Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Description: Program that prints all single digit numbersfrom 0, followed by a new line.
  * Return: 0
 */
 
