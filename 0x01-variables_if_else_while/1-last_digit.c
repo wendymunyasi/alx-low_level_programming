bcb009466ee9bd488eb1b70dbd091e5ce17904cc
wendymunyasi <wendymunyasi@gmail.com>
2022-03-11 13:11:42 +0000

    Fix errors in the last digit prog

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,8 +14,8 @@ int n;
 int last_digit;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-last_digit = abs(n % 10);
+n = -980;
+last_digit = n % 10;
 if (last_digit > 5)
 {
 printf("Last digit of %i is %d and is greater than 5\n", n, last_digit);
