e002399411a1f352726ee2ac5c8d3ee107f7d354
wendy munyasi <wendymunyasi@gmail.com>
2022-03-14 23:18:31 +0300

    Last try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1); }
