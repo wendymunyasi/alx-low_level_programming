d099a4dea9753b35ef2be8ec4f123d059aafabfd
wendy munyasi <wendymunyasi@gmail.com>
2022-03-15 23:48:10 +0300

    Add a program that prints 9x9 time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * times_table- A fucntion that prints the 9 times
- * table all over
- * Return: no return
-*/
+ * times_table - prints times table
+ * Return:void
+ */
 
 void times_table(void)
 {
-int a, b, product;
 
+int a = 0;
+int b;
+int rep;
+
+while (a <= 9)
+{
+b = 0;
+while (b <= 9)
+{
+rep = a * b;
+if (b == 0)
+{
+_putchar('0' + rep);
+}
+else if (rep < 10)
+{
+_putchar(' ');
+_putchar('0' + rep);
+}
+else
+{
+_putchar('0' + rep / 10);
+_putchar('0' + rep % 10);
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+b++;
+}
+_putchar('\n');
+a++;
+}
 }
