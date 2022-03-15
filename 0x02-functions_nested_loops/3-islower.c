6586fdeaace5535178f9479372c6c11747ef3325
wendy munyasi <wendymunyasi@gmail.com>
2022-03-15 17:07:00 +0300

    Add a program that checks if a letter is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - Checks if the lettr c is in lowercase
+ * Description: Checks if the lettr c is in lowercase
+ * Return: 0 or 1 depending on the answer
+ * @c: just a letter
+*/
+
+int _islower(int c)
+{
+if islower(c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
