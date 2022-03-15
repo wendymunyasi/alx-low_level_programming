23c391e90fe77528de68a49d3b44927b3dbc76cc
wendy munyasi <wendymunyasi@gmail.com>
2022-03-15 16:40:37 +0300

    Add a C program that writes alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,26 @@
 #include <ctype.h>
 #include "main.h"
-#include "main.h"
-#include "main.h"
-#include "main.h"
-#include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
- * Description: prints alphabets is lower case followed
+ * print_alphabet_x10 - Prints the alphabet in lowercase.
+ * Description: prints alphabets in lower case 10 times
  * by new line
  * Return: 0
 */
 
-void print_alphabett_x10(void)
+void print_alphabet_x10(void)
 {
 
-int x;
+int x, a;
 int lower_x;
+
+for (a = 0; a <= 9; a++)
+{
 for (x = 'A'; x <= 'Z'; x++)
 {
 lower_x = tolower(x);
 _putchar(lower_x);
 }
-_putchar('\n'); }
+_putchar('\n');
+}
+}
