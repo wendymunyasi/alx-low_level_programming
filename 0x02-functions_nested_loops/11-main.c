93c4d39a7d05f2741a57f177413fe28ba5c3a5cc
wendy munyasi <wendymunyasi@gmail.com>
2022-03-15 23:36:19 +0300

    Add a program that prints values to 98

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+    return (0);
+}
