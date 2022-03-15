85e791555d62eb8b9504799262f110317cb66c54
wendy munyasi <wendymunyasi@gmail.com>
2022-03-15 18:32:01 +0300

    Add program that returns abs value of num

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs- A fucntion that computes the absolute
+ * value of an integer
+ * @n: integer number
+ * Return: 0 or 1 depending on the answer
+*/
+
+int _abs(int n)
+{
+    return abs(n);
+}
