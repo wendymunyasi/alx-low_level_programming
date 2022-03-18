8956121e238dd1ba3a6d3ed0db1b8dba8b0fe7cd
wendy munyasi <wendymunyasi@gmail.com>
2022-03-19 00:46:15 +0300

    Add a function that checks isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+ #include "main.h"
+
+/**
+ * _isupper - check if the character is UpperCase
+ * @c: the character to be checked
+ * Return: 1 if lower, 0 if capital
+ */
+
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
