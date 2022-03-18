4732e1c9835703f6ac029cd3718cf0a4fa124011
wendy munyasi <wendymunyasi@gmail.com>
2022-03-19 01:00:22 +0300

    Add a func that prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ ** print_line - print character ' _'
+ ** @n: number of time to print character
+ ** Return: 0 (success)
+ */
+
+void print_line(int n)
+{
+
+int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+
+}
+
+_putchar('\n');
+}
