547a54c4fcc200a2be3d063ac2efb9f9622f64dc
wendy munyasi <wendymunyasi@gmail.com>
2022-03-20 16:11:35 +0300

    Betty style fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,29 +9,29 @@
 
 int main(void)
 {
-    int hundreds, tens, ones;
+int hundreds, tens, ones;
 
-    for (hundreds = 0; hundreds <= 9; hundreds++)
-    {
-        for (tens = hundreds + 1; tens <= 9; tens++)
-        {
-            for (ones = tens +1; ones <= 9; ones++)
-            {
-                putchar(hundreds + '0');
-                putchar(tens + '0');
-                putchar(ones + '0');
+for (hundreds = 0; hundreds <= 9; hundreds++)
+{
+for (tens = hundreds + 1; tens <= 9; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(hundreds + '0');
+putchar(tens + '0');
+putchar(ones + '0');
 
-               if(hundreds < 7)
-               {
-                   putchar(',');
-                   putchar(' ');
-               }
-            }
+if  (hundreds < 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
 
-        }
+}
 
-    }
-    putchar('\n');
+}
+putchar ('\n');
 
-    return(0);
+return  (0);
 }
