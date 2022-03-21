5b2a1a14909074654b508de159da82bb8b9dbfb6
wendy munyasi <wendymunyasi@gmail.com>
2022-03-21 14:35:52 +0300

    Fix task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -37,3 +37,19 @@ void rev_string(char *s)
 		end--;
 	}
 }
+
+/**
+ * string_length - function that reverses a string
+ * @s: string to be reversed
+ * Return: 0 string in reversed
+ */
+
+int string_length(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+		length++;
+
+	return (length);
+}
