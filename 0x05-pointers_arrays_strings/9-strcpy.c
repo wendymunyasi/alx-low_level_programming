d8313d748668c89a6a54acb9dd79fc76b9558313
wendy munyasi <wendymunyasi@gmail.com>
2022-03-21 17:18:23 +0300

    Fix betty style and doc errors

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,14 +2,18 @@
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * _strcpy - A func that copies a string
+ * Return: A pointer to *dest.
+ * @dest: Final destination for the string
+ * @src: Initial destination for the string
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	char *c = dest;
 
-	while ((*dest++ = *src++));
+	while
+	((*dest++ = *src++));
+
 	return (c);
 }
