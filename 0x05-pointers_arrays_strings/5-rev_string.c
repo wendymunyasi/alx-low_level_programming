ad725667ed6987ac79f98afd018493d8f463f5d8
wendy munyasi <wendymunyasi@gmail.com>
2022-03-21 14:20:04 +0300

    First try for 5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * rev_string - function that reverses a string
+ * string_length - function that reverses a string
+ * @s: string to be reversed
+ * Return: 0 string in reversed
+ */
+
+void rev_string(char *s)
+{
+	int len, i;
+	char *begin, *end, temp;
+
+	len = string_length(s); /* Get function from the string_length.c */
+	begin = s;
+	end = s;
+
+	for (i = 0; i < len - 1; i++)
+		end++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		/**
+		 * The idea is to swap the beginning and end pointers
+		 * of the string. Instead of swapping the elements, here
+		 * , we will swap the pointers where the addresses of the
+		 * input string are stored.
+		 */
+
+		temp   = *end;
+		*end   = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,3 +11,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int string_length(char*);
