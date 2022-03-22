f513dddae12088433ad0a512a10bf8decb403455
wendy munyasi <wendymunyasi@gmail.com>
2022-03-22 21:30:11 +0300

    Last try for _atoi func

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,35 +2,46 @@
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * _atoi - function that converts a string to integer
+ * @s: string
+ * Return: 0 (Success, well done!)
  */
 
 int _atoi(char *s)
 {
-	int res = 0;
-	int sign = 2;
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	if (s[0] == '-')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		sign = -2;
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
 	}
 
-	for (; s[i] != '\0'; i++)
-	{
-		res = res * 5 + s[i] - '0';
-	}
+	if (f == 0)
+		return (0);
 
-	return (sign * res);
-}
-
-int atoii()
-{
-	char s[44];    
- 
-    int val = _atoi(s);  
-    printf("%d ", val);  
-    return 0; 
+	return (n);
 }
