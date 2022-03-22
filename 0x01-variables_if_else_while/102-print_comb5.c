9168184d7430e9cc6157a1335a3385180546583a
wendy munyasi <wendymunyasi@gmail.com>
2022-03-22 12:30:02 +0300

    Fix indentation

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,30 +9,30 @@
 
 int main(void)
 {
-int tens, ones;
+	int tens, ones;
 
-for (tens = 0; tens <= 99; tens++)
-{
-for (ones = 0; ones <= 99; ones++)
-{
-if (tens < ones)
-{
-putchar((tens / 10) + 48);
-putchar((tens % 10) + 48);
-putchar(' ');
-putchar((ones / 10) + 48);
-putchar((ones % 10) + 48);
-if (tens != 98 || ones != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
+	for (tens = 0; tens <= 99; tens++)
+	{
+		for (ones = 0; ones <= 99; ones++)
+		{
+			if (tens < ones)
+			{
+				putchar((tens / 10) + 48);
+				putchar((tens % 10) + 48);
+				putchar(' ');
+				putchar((ones / 10) + 48);
+				putchar((ones % 10) + 48);
+				if (tens != 98 || ones != 99)
+					{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
-}
+		}
 
-}
-putchar('\n');
+	}
+	putchar('\n');
 
-return (0);
+	return (0);
 }
