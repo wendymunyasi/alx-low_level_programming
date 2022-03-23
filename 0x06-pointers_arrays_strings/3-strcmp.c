c868fed8b97ecafbbe55c6b103bc8bc87f898b1f
wendy munyasi <wendymunyasi@gmail.com>
2022-03-23 18:46:29 +0300

    Add a func that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: pointer to null-terminated first string
+ * @s2: pointer to null-terminated second string
+ * Return: 0 (Success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
