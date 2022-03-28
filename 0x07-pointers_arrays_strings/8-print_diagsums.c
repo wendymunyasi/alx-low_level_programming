212b34a6b0d44b84692db2104aa8048ec37713c8
wendy munyasi <wendymunyasi@gmail.com>
2022-03-28 17:13:08 +0300

    Add a func that prints sum of two diagonals in matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - function that prints sum of the two diagonals
+ * of a square matrix of integers.
+ * @a: pointer to array of characters.
+ * @size: size of array
+ *
+ * Return: no return.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, max_size;
+	int prinDiagonalSum = 0;
+	int secDiagonalSum = 0;
+
+	max_size = size * size;
+
+	for (i = 0; i < max_size; i += size + 1)
+		prinDiagonalSum += a[i];
+
+	for (i = 0; i < max_size - 1; i += size - 1)
+		secDiagonalSum += a[i];
+
+	printf("%d, %d\n", prinDiagonalSum, secDiagonalSum);
+}
