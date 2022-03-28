7ef8b754a6dff4f4538d77366e356d80860e4913
wendy munyasi <wendymunyasi@gmail.com>
2022-03-28 10:01:49 +0300

    Add a func that locates a character in a str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ **_strchr - function that locates a character in a string
+ * @s: pointer to the null-terminated byte string to be analyzed
+ * @c: character to search for
+ *
+ * Return:  a pointer to first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	/* loop through the string 8*/
+	while (*s)
+	{
+		/* check if a characer at particular index = c */
+		if (*(s + i) == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (0);
+}
