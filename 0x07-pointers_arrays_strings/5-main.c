6ce1e68bd552e4cd767b6a8143d0110e4ac17bf9
wendy munyasi <wendymunyasi@gmail.com>
2022-03-28 14:20:15 +0300

    Add a func that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "Techie Delight Ace the Technical Interviews";
+    char *f = "Ace";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
+}
