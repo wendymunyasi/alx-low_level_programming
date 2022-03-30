cdbfb2bb0a8f7a20c7250baf097f5274d8a340f1
wendy munyasi <wendymunyasi@gmail.com>
2022-03-30 21:05:40 +0300

    Add a func that compares two strings

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,11 +3,12 @@
 #include  "2-strlen_recursion.c"
 
 /**
- * checker - checks if string is palindrome
+ * checker - checks if string is palindrome or not.
  * @s: pointer to the null-terminated byte string to be examined.
- * @length: highest index of string
- * @a: lowest index of string
- * Return: 1 if true, 0 if not
+ * @length: highest index of string.
+ * @a: lowest index of string.
+ * 
+ * Return: 1 if true, 0 if not.
  */
 
 int checker(char *s, int length, int a)
