346c5e15c47790e82ae8460a97be12f613ed5b4e
wendy munyasi <wendymunyasi@gmail.com>
2022-04-01 22:19:34 +0300

    HotFix2

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
+#include <unistd.h>
+
+/**
+ * main - Main entry point for the function.
+ *
+ * Return: 0 on success.
+ */
+
 int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+    
+	return (0);
 }
