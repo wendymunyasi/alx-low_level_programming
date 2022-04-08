fb261d560bfdbd43324fe333c0f5e09d028baf5c
wendy munyasi <wendymunyasi@gmail.com>
2022-04-08 14:09:10 +0300

    Add a func that allocates mem using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * malloc_checked - fucntion that allocates memory using malloc.
+ * @b: Number of bytes to allocate.
+ *
+ * If malloc fails, the malloc_checked function should cause normal,
+ * process termination with a status value of 98.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	void *p;
+	
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
