93e3de016cf20fb816e82ed59a0d226873d63bf6
wendy munyasi <wendymunyasi@gmail.com>
2022-04-11 22:58:32 +0300

    Fix indentation and minor issues

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,22 +20,23 @@
 
 int main(void)
 {
-int n;
-int last_digit;
+    int n;
+    int last_digit;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-last_digit = n % 10;
-if (last_digit > 5)
-{
-printf("Last digit of %i is %d and is greater than 5\n", n, last_digit);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    last_digit = n % 10;
+    if (last_digit > 5)
+    {
+        printf("Last digit of %i is %d and is greater than 5\n", n, last_digit);
+    }
+    else if (last_digit == 0)
+    {
+        printf("Last digit of %i is %d and is 0\n", n, last_digit);
+    }
+    else if (last_digit < 6 && last_digit != 0)
+    {
+        printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_digit);
+    }
+    return (0);
 }
-else if (last_digit == 0)
-{
-printf("Last digit of %i is %d and is 0\n", n, last_digit);
-}
-else if (last_digit < 6 && last_digit != 0)
-{
-printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_digit);
-}
-return (0); }
