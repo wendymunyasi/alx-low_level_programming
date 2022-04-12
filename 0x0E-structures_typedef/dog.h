906c76da18ea4757970fb3543c17505415f51cc3
wendy munyasi <wendymunyasi@gmail.com>
2022-04-13 00:44:43 +0300

    Add a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,24 @@
-#ifndef DOG_H_
-#define DOG_H_
+#ifndef _DOG_H_
+#define _DOG_H_
 
-#define SUM(x, y) ((x) + (y))
+/**
+ * struct dog - defines a new type struct dog.
+ * @name: pointer to name of the dog.
+ * @age: age of the dog.
+ * @owner: pointer to owner of the dog.
+ * @dog_t: type def for struct dog.
+ */
 
-#endif /* DOG_H_ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* _DOG_H_ */
