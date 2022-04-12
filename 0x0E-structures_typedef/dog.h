f8a241ad539dd96434d2ec7d4b63378230307ab8
wendy munyasi <wendymunyasi@gmail.com>
2022-04-13 00:49:59 +0300

    Fix betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,29 @@
 #define _DOG_H_
 
 /**
+ * @dog_type - typedef for struct dog
+ *
+ */
+
+typedef struct dog dog_type;
+
+/**
  * struct dog - defines a new type struct dog.
  * @name: pointer to name of the dog.
  * @age: age of the dog.
  * @owner: pointer to owner of the dog.
- * @dog_t: type def for struct dog.
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+dog_type *new_dog(char *name, float age, char *owner);
+void free_dog(dog_type *d);
 
 #endif /* _DOG_H_ */
