2ff0bb73f5930eaf2a4e6ea4f210ee852d832ff4
wendy munyasi <wendymunyasi@gmail.com>
2022-04-14 14:55:52 +0300

    Fix betty coding style

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
-#ifndef MAIH_H
-#define MAIN_H
- 
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* _MAIN_H_ */
