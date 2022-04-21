230296901fb623911c8b48b2d360c1c236bfc1b3
Wendy Munyasi <Wendymunyasi@gmail.com>
2022-04-22 02:22:48 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -17,25 +17,25 @@
 ; }
 
 ; Declare needed C functions
-			extern printf               ; the C function to be called
+		extern printf			; the C function to be called
 
-			SECTION .data               ; Data section, initialized variables
-message:    db "Hello, Holberton", 0    ; strings must be terminated with 0 in C
-format:     db "%s", 10, 0              ; the printf format, "\n", '0'
+		SECTION .data			; Data section, initialized variables
+message:    	db "Hello, Holberton", 0	; strings must be terminated with 0 in C
+format:    	db "%s", 10, 0			; the printf format, "\n", '0'
 
-			SECTION .text
+		SECTION .text
 
-			global main
-main:                                   ; the program label for the entry point
-			push rbp                 	; set up stack frame, must be alligned
+		global main
+main:						; the program label for the entry point
+		push rbp			; set up stack frame, must be alligned
 
-			mov rdi,format
-			mov rsi,message
-			mov rax,0		        	; or can be  xor  rax,rax
-			call printf		        	; Call C function
+		mov rdi,format
+		mov rsi,message
+		mov rax,0			; or can be  xor  rax,rax
+		call printf			; Call C function
 
-			pop rbp		            	; restore stack
+		pop rbp				; restore stack
 
-			mov rax,0		        	; normal, no error, return value
-			ret			                ; return
+		mov rax,0			; normal, no error, return value
+		ret				; return
 
