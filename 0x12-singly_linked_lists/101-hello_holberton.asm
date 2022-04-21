6c9e4b184394bc309a922474e9f0108bcc542a75
wendy munyasi <wendymunyasi@gmail.com>
2022-04-22 02:17:36 +0300

    Add a 64-bit prog that prints a string

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -27,14 +27,15 @@ format:     db "%s", 10, 0              ; the printf format, "\n", '0'
 
 			global main
 main:                                   ; the program label for the entry point
-			push    rbp                 ; set up stack frame, must be alligned
+			push rbp                 	; set up stack frame, must be alligned
 
-			mov	    rdi,format
-			mov	    rsi,message
-			mov	    rax,0		        ; or can be  xor  rax,rax
-			call    printf		        ; Call C function
+			mov rdi,format
+			mov rsi,message
+			mov rax,0		        	; or can be  xor  rax,rax
+			call printf		        	; Call C function
 
-			pop	    rbp		            ; restore stack
+			pop rbp		            	; restore stack
 
-			mov	    rax,0		        ; normal, no error, return value
+			mov rax,0		        	; normal, no error, return value
 			ret			                ; return
+
