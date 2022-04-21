bd0f6d63219df9b8432f3d6242fbeb17ce72d73f
wendy munyasi <wendymunyasi@gmail.com>
2022-04-22 00:26:38 +0300

    Fix #1

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,6 +13,9 @@
 void free_list(list_t *head)
 {
 	list_t *temp_variable;
+	/* assign temp_variable to head */
+	temp_variable = head;
+
 	/* check if head is NULL if yes the list is empty and we just return */
 	while (head != NULL)
 	{
@@ -20,7 +23,11 @@ void free_list(list_t *head)
 		temp_variable = head;
 		/* make the head point to the next node on list */
 		head = head->next;
+		/* free the str */
+		free(temp_variable->str);
 		/* free temp_variable and head just points to the rest of the list */
 		free(temp_variable);
 	}
+	/* free the head */
+	free(head);
 }
