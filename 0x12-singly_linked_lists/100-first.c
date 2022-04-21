ceec975e436420716fb0a10cfeb246c9d03eecc7
wendy munyasi <wendymunyasi@gmail.com>
2022-04-22 00:40:37 +0300

    Add a func that exec before the main func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "lists.h"
+
+/**
+ * before_main - funcion that prints a specific string before the main,
+ * function in executed.
+ *
+ * Return: no return.
+ */
+
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("nI bore my house upon my back!\n");
+}
