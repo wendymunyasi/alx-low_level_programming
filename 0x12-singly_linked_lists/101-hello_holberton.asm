296f3e1dd091f51d8a040ce1021a6f5155a00c08
wendy munyasi <wendymunyasi@gmail.com>
2022-04-22 02:08:54 +0300

    Add a 64-bit prog that prints a string

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,40 @@
+; Filename: 101-hello_holberton.asm - Print a string using printf
+; Author: Wendy Munyasi
+; Assemble: nasm -f elf64 101-hello_holberton.asm
+; Link: gcc -no-pie -std=gnu89 101-hello_holberton.o -o hello
+; Run: ./hello
+; You are only allowed to use printf
+; You are not allowed to use interrupts
+; Your program will be compiled using nasm and gcc
+
+; Equivalent C code
+; #include <stdio.h>
+; int main(void)
+; {
+; 	char msg[] = "Hello, Holberton";
+;   printf("%s\n",msg);
+;   return (0);
+; }
+
+; Declare needed C functions
+			extern printf               ; the C function to be called
+
+			section .data               ; Data section, initialized variables
+message:    db "Hello, Holberton", 0    ; strings must be terminated with 0 in C
+format:     db "%s", 10, 0              ; the printf format, "\n", '0'
+
+			section .text
+
+			global main
+main:                                   ; the program label for the entry point
+			push    rbp                 ; set up stack frame, must be alligned
+
+			mov	    rdi,format
+			mov	    rsi,message
+			mov	    rax,0		        ; or can be  xor  rax,rax
+			call    printf		        ; Call C function
+
+			pop	    rbp		            ; restore stack
+
+			mov	    rax,0		        ; normal, no error, return value
+			ret			                ; return
