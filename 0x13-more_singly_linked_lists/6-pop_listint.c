74f3d51c2dbeaac61aca4061dd77f7d7ba2b0568
wendy munyasi <wendymunyasi@gmail.com>
2022-04-26 17:28:20 +0300

    Add a func that deletes a node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - function that deletes the head node of a listint_t,
+ * linked list.
+ * listint_t linked list.
+ * @head: pointer to pointer to the head of linked list.
+ *
+ * Make sure there is no memory leaks.
+ *
+ * Return:  and returns the head node’s data (n) or 0 if list is empty.
+ */
+
+int pop_listint(listint_t **head)
+{
+    listint_t *temp_variable; /* keep track of current head */
+    int data = 0;
+
+    if (head == NULL || *head == NULL)
+        return (0);
+    temp_variable = *head; /* stores the pointer to temp_variable head */
+    data = temp_variable->n;
+    *head = (*head)->next;
+    free(temp_variable);
+    return (data);
+}
