971767f64d0f363010430529f0293ea051afafff
wendy munyasi <wendymunyasi@gmail.com>
2022-04-26 20:48:45 +0300

    Fix betty style

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,7 +6,6 @@
  * index of a listint_t linked list.
  * @head: pointer to pointer to the head of linked list.
  * @index:index of the node that should be deleted. Index starts at 0.
- * @n: value of the new node.
  *
  * Return:  1 if it succeeded, -1 if it failed.
  */
@@ -39,7 +38,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp_variable2 = temp_variable->next;
 		temp_variable->next = temp_variable2->next;
 		free(temp_variable2);
-		return(1);
-	}/* by here the code has failed so we return -1 */
-	return(-1);
+		return (1);
+	} /* by here the code has failed so we return -1 */
+	return (-1);
 }
