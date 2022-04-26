93d455821e3461dc30eebc2db45d13c88fe8c549
wendy munyasi <wendymunyasi@gmail.com>
2022-04-26 17:09:55 +0300

    Add a func that frees a sinlglyy linked listt

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * free_listint - function that frees a singly linked list.
+ * free_listint2 - function that frees a singly linked list.
  * @head: pointer to pointer to the head of linked list.
  *
  * Make sure there is no memory leaks.
@@ -13,17 +13,16 @@
 
 void free_listint2(listint_t **head)
 {
-    listint_t *temp_variable;
+	listint_t *temp_variable;
 
-    if (head == NULL)
-        return; /* do not do anything */
-    /* now assign value at head to trav */
-    while (*head != NULL)
-    {
-        temp_variable =  *head;
-        *head = ( *head)->next;
-        free(temp_variable);
-    }
-    *head = NULL;
-    /* make sure that value at head is null */
+	if (head == NULL)
+		return; /* do not do anything */
+	while (*head != NULL)
+	{
+		temp_variable =  *head;
+		*head = (*head)->next;
+		free(temp_variable);
+	}
+	/* *head = NULL; */ /* not needed */
+	/* make sure that value at head is null */
 }
