29e200e1dbf08275e87174a0e4d55b25b7c22073
wendy munyasi <wendymunyasi@gmail.com>
2022-04-26 16:38:41 +0300

    Add a func that frees a linkedlist by recursion

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - function that frees a singly linked list.
+ * @head: pointer to the head of linked list.
+ *
+ * Here we will use recursion unlike in the previous task.
+ * Make sure there is no memory leaks.
+ *
+ * Return: No return.
+ */
+
+void free_listint(listint_t *head)
+{
+	/* go to the end of the list then start freeing from there */
+
+	/* we have reached the end of the list */
+	if (head == NULL)
+		return;
+	/* free everything else */
+	free_listint(head->next);
+	/* then free head */
+	free(head);
+	/* elegante, eh 👏*/
+}
