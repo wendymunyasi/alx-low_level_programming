49436d8daecc15f34dbf759f50006db70957f3aa
wendy munyasi <wendymunyasi@gmail.com>
2022-04-27 15:23:25 +0300

    Attemp to reverse a linked loist

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * listint_t - function that reverses a listint_t linked list.
+ * @head: pointer to pointer to the head of linked list.
+ *
+ * You are not allowed to use more than 1 loop.
+ * You are not allowed to use malloc, free or arrays.
+ * You can only declare a maximum of two variables in your function.
+ *
+ * Return:  the linked list in reverse.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous;
+	listint_t *current = *head;
+	
+	/* traverse the list */
+	while (current != NULL)
+	{
+		struct listint_s *next = current->next;
+		/* fix the current node */
+		current->next = previous;
+		/* adjust the two pointers */
+		previous = current;
+		current = next;
+	}
+	/* fix the head pointer to point to the new front */
+	*head = previous;
+	return (*head);
+}
