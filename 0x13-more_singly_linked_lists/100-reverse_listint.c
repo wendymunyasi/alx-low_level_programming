226371ca9c0614c7d173ea80c884a109911ee2a0
wendy munyasi <wendymunyasi@gmail.com>
2022-04-27 16:21:36 +0300

    Add comments to code

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -14,13 +14,14 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
+	/*variable to keep track of the current head */
 	listint_t *current = *head;
 
+	/* if we have no list or ptr to ptr to the head of the list */
 	if (head == NULL || *head == NULL)
 	{
 		return (NULL);
 	}
-
 	/* check if the next field of current is null */
 	if (current->next == NULL)
 	{
@@ -28,15 +29,22 @@ listint_t *reverse_listint(listint_t **head)
 		*head = current;
 		return (*head);
 	}
-
+	/* if we have pointer to first node and next field of pointer */
+	/* to first node */
 	else if (*head && (*head)->next)
 	{
+		/* declare a temp variable and assign it as first node */
 		listint_t *temp_variable = *head;
+		/* access the next field */
 		*head = (*head)->next;
+		/* reverse it recursively */
 		reverse_listint(head);
+		/* access the data at the memory location to which temp_variable->next */
+		/* points to and set it as current */
 		temp_variable->next->next = current;
+		/* access the next field of temp_variable and set it as NULL */
 		temp_variable->next = NULL;
 	}
+	/* return the pointer to the first node */
 	return (*head);
 }
-
