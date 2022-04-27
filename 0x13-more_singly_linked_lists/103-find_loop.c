a40f980613279a17517a27cfda9cfd1039f4bd48
wendy munyasi <wendymunyasi@gmail.com>
2022-04-27 21:14:48 +0300

    Attempt to fix task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -15,30 +15,28 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	/* Use of Floyd’s Cycle-Finding Algorithm */
-	listint_t *slow, *fast;
-	/* let slow and fast be two node pointers pointing to the head node */
-	slow = fast = head;
+	listint_t *slow = head, *fast = head;
 
-	while (slow && fast && fast->next)
-	{
-		/* in every iteration slow ptr moves ahead by one node */
-		slow = slow->next;
-		/* in every iteration fast ptr moves ahead by two nodes */
-		fast = fast->next->next;
-		/**
-		 *
-		 * eventually meet at the same node, thus indicating that the,
-		 * linked list contains a loop.
-		 */
-		if (slow == fast)
-		{
-			printf("Loop starts at [&p]");
-			/* return the head */
-			return (head); /* return 1 */
-		}
-		/* return NULL if there is no loop meaning the ptrs did not meet */
+	if (head == NULL || head->next == NULL)
 		return (NULL);
+	slow = slow->next;
+	fast = fast->next->next;
+
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+			break;
+		slow = slow->next;
+		fast = fast->next->next;
 	}
-	return (0);
+	if (slow != fast)
+		return (NULL);
+
+	slow = head;
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (slow);
 }
