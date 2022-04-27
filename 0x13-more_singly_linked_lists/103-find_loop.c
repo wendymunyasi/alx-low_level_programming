94aa18e376904feb8ae08203be81a574d5c7f9b2
wendy munyasi <wendymunyasi@gmail.com>
2022-04-27 16:56:09 +0300

    Add a function which detects a loopsieee

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,31 +14,28 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	/**
-	 * Algorithm to detect cycle in a linked list.
-	 * Let "head" be the head pointer of given linked list.
-	 * Let, "slow" and "fast" be two node pointers pointing to the head,
-	 * node of linked list.
-	 * In every iteration, the "slow" pointer moves ahead by one,
-	 * node(slow = slow->next;) whereas "fast" pointer moves two nodes at,
-	 * a time(fast = fast->next->next;).
-	 * If linked list contains a loop, "slow" and "fast" pointers will,
-	 * eventually meet at the same node, thus indicating that the linked,
-	 * list contains a loop.
-	 * If pointers do not meet then linked list doesn’t have loop.
-	 * This algorithm is known as Floyd’s Cycle-Finding Algorithm
-	 */
+	/* Use of Floyd’s Cycle-Finding Algorithm */
 	listint_t *slow, *fast;
+	/* let slow and fast be two node pointers pointing to the head node */
 	slow = fast = head;
 
 	while (slow && fast && fast->next)
 	{
+		/* in every iteration slow ptr moves ahead by one node */
 		slow = slow->next;
+		/* in every iteration fast ptr moves ahead by two nodes */
 		fast = fast->next->next;
+		/**
+		 *
+		 * eventually meet at the same node, thus indicating that the,
+		 * linked list contains a loop.
+		 */
 		if (slow == fast)
 		{
+			/* return the head */
 			return (head);
 		}
+		/* return NULL if there is no loop meaning the ptrs did not meet */
 		return (NULL);
 	}
 }
