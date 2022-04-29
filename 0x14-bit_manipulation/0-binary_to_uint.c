f14d6ee5beb8480c06db4713b96979ee0bbfca96
wendy munyasi <wendymunyasi@gmail.com>
2022-04-29 15:39:30 +0300

    Add a func that converts a bin to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int.
+ * @b: pointer to a string of 0 and 1 chars.
+ *
+ * b is NULL.
+ *
+ * Return: the converted number or 0 if there is one or more chars in the,
+ * string b that is not 0 or 1.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, usigned_int = 0, dec_val = 1;
+	/* if we have no string return 0 */
+	if (!b)
+		return (0);
+
+	/* loop from end of string (-1 to ignore '\0' ) to start of string */
+	for (i = _strlen(b) - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			usigned_int += dec_val;
+		}
+		dec_val *= 2;
+	}
+	return (usigned_int);
+}
+
+/**
+ * _strlen - Function that returns the length of string.
+ * @s: pointer to the null-terminated byte string to be examined.
+ *
+ * Return: The length of the null-terminated byte string s.
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,9 +2,11 @@
 #define _MAIN_H_
 
 unsigned int binary_to_uint(const char *b);
+int _strlen(const char *s);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-#endif _MAIN_H_ /* _MAIN_H_ */
+
+#endif
