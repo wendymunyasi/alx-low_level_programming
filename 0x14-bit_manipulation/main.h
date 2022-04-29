2e046b8fe0dfe88a8bd3385bc0e00c7e78928b66
wendy munyasi <wendymunyasi@gmail.com>
2022-04-29 14:57:37 +0300

    Set up project

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+#endif _MAIN_H_ /* _MAIN_H_ */
