7f465af530e339103bacae95bb200c66fb38261b
wendy munyasi <wendymunyasi@gmail.com>
2022-04-30 13:47:58 +0300

    Add a func that returns flipped bits count

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * flip_bits - func that returns no. of bits needed to flip to get,
+ * from one number to another.
+ * @n: unsigned long integer.
+ * @m: unsigned long integer.
+ *
+ * You are not allowed to use the % or / operators.
+ *
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	/* take XOR of n and m and store in XOR_nm */
+	int XOR_nm = n ^ m;
+
+	/* Using Brian Kernighan’s algorithm to count set bits */
+	/* count stores the total bits set in XOR_nm */
+	while (XOR_nm)
+	{
+		/* clear the least significant bit */
+		XOR_nm = XOR_nm & (XOR_nm - 1);
+		count++;
+	}
+	/* return count */
+	return (count);
+}
