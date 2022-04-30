5b8c33d76acbfc4b38f1c60d094a327f23abbe2d
wendy munyasi <wendymunyasi@gmail.com>
2022-04-30 14:07:39 +0300

    Attempt to fix check 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,15 +15,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
 	/* take XOR of n and m and store in XOR_nm */
-	unsigned int XOR_nm = n ^ m;
+	unsigned int XOR_nm;
 
 	/* Using Brian Kernighan’s algorithm to count set bits */
 	/* count stores the total bits set in XOR_nm */
-	while (XOR_nm)
-	{
-		count += (XOR_nm & 1);
-		XOR_nm >>= 1;
-	}
+	for (XOR_nm = n ^ m; XOR_nm != 0; XOR_nm = XOR_nm >> 1)
+		count += XOR_nm & 1;
 	/* return count */
 	return (count);
 }
