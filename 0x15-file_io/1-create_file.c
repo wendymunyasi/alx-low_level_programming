c65edc0d2ddb76dc999e5fe607ee64ec2661990b
wendy munyasi <wendymunyasi@gmail.com>
2022-05-04 16:13:43 +0300

    Attempt to fix check 7 - 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,17 +23,11 @@ int create_file(const char *filename, char *text_content)
 
 	/* if filename is NULL */
 	if (!filename)
-	{
 		return (-1);
-	}
-	else
-	{
-		file_descriptor = open(filename, O_TRUNC, 0600);
-	}
 
 	/* if text_content is NULL create an empty file */
 	if (!text_content)
-		file_descriptor = open(filename, O_CREAT, 0600);
+		text_content = "";
 
 	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
