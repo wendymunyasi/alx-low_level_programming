df95aa7d9a4f926b3a1dd50e3faa924374111483
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 17:53:50 +0300

    Add a func that returns elements of dbl linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - function that prints all the elemts of a doubly linked list
+ * dlistint_len - function that returns the number of elements in a,
+ * linked dlistint_t list.
  * @h: poiner to the head of the list
  *
- * Return: number of nodes
+ * Return: number of elements
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	/* while we have a pointer to the list */
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
