e8e939ae1f76a245bdb085290826a9aa23c6eebb
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 21:24:26 +0300

    Fix indentation issues in 1-main.c

--- 0x17-doubly_linked_lists/1-main.c
+++ 0x17-doubly_linked_lists/1-main.c
@@ -10,25 +10,25 @@
  */
 int main(void)
 {
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
+	dlistint_t *head;
+	dlistint_t *new;
+	dlistint_t hello = {8, NULL, NULL};
+	size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = dlistint_len(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
+	head = &hello;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (EXIT_FAILURE);
+	}
+	new->n = 9;
+	head->prev = new;
+	new->next = head;
+	new->prev = NULL;
+	head = new;
+	n = dlistint_len(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (EXIT_SUCCESS);
 }
