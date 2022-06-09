2e56dc615a671f5af0aa932153eddb4f147f9ba8
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 21:27:18 +0300

    Add a func that inserts a new node at given pos

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,10 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* check if idx = 0 */
 	if (idx == 0)
 	{
-		/* access the next field of new_node and assign it as first node */
-		new_node->next = *h;
-		*h = new_node;
-		return (new_node);
+		new_node = add_dnodeint(h, n);
 	}
 	/* make traverse be the value at head */
 	traverse = *h;
