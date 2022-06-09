1a578893fe2ef6854a434b4e57f69e013299fa6e
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 17:16:17 +0300

    Add a func that prints all elemts of dbl linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elemts of a doubly linked list
+ * @h: poiner to the head of the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	/* while we have a pointer to the list */
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
