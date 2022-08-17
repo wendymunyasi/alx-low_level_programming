76209ea78ed11218fd1bc97d33d67bddd51cd5b6
wendy munyasi <wendymunyasi@gmail.com>
2022-08-17 22:58:02 +0300

    Add function that returns grid perim

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function island_perimeter(grid)"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid.
+
+    Args:
+        grid (_type_): grid
+    """
+    m, n = len(grid), len(grid[0])
+    land, neighbour = 0, 0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                land += 1
+                if i < m - 1 and grid[i+1][j] == 1:
+                    neighbour += 1
+                if j < n - 1 and grid[i][j + 1] == 1:
+                    neighbour += 1
+    return land * 4 - 2 * neighbour
