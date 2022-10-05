fede4cbce18973be6613766268e3291a6a596cc8
wendy munyasi <wendymunyasi@gmail.com>
2022-10-05 12:41:45 +0300

    Add a comment

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
+		if (array[i] == value) /* if (*(array + i) == value) */
 			return (i);
 	}
 	return (-1);
