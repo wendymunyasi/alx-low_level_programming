0361abc241aecfdf731ffe83b744c4b4f7b56b88
wendy munyasi <wendymunyasi@gmail.com>
2022-10-05 14:09:29 +0300

    Add prototypes to header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int binary_search_index(int *array, size_t size, size_t l, size_t r, int value);
+void print_array(int *array, size_t l, size_t r);
+int binary_search_index(int *array, size_t l, size_t r, int value);
 
 #endif /* _search_algos_ */
