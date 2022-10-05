2aecbe6a2db1fd0983efa002cdfef841b0978430
wendy munyasi <wendymunyasi@gmail.com>
2022-10-05 12:31:25 +0300

    Add function that uses Linear search algo - task 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _search_algos_
+#define _search_algos_
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _search_algos_ */
